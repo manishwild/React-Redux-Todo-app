038d55d133f77c29d0a04188188173c5e56f3e9f
manish <manishwild1000@gmail.com>
2020-12-13 17:35:59 +0100

    day 2

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className='App'>
+        <nav>
+            <section>
+                <h1>React Redux Todo App </h1>
+                <div className='navContent'>
+                <div className='navLinks'></div>
+                </div>
+            </section>
+        </nav>
+        <section>
+        <h2>React Redux to do</h2>
+      </section>
+        </div>
+    )
+}
+
+export default App
